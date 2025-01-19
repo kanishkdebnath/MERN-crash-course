42127c0eb5548afe3afc1d57b971cd9560f8df86
Kanishk Debnath <134499777+kanishkdebnath@users.noreply.github.com>
2025-01-20 01:32:37 +0530

    Add Update Product endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import Product from './model/product.model.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
 const app = express();
@@ -67,6 +68,43 @@ app.delete("/api/products/:id", async (req, res) => {
     })
   } catch (error) {
     console.error(`Error in deleting product: ${error.message}`)
+    return res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+})
+
+
+app.put("/api/products/:id", async (req, res) => {
+  const {id} = req.params;
+  
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({
+      success: false,
+      message: 'Invalid ID or Product ID does not exist'
+    });
+  }
+  
+  const product = req.body;
+
+  if (!product || !product.name || !product.price || !product.image) {
+    console.log(product);
+    
+    return res.status(400).json({
+      success: false,
+      message: "Provide all fields",
+    });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {new: true});
+    return res.status(201).json({
+      success: true,
+      data: updatedProduct,
+    });
+  } catch (error) {
+    console.error(`Error in updating product: ${error.message}`)
     return res.status(500).json({
       success: false,
       message: "Server error",
